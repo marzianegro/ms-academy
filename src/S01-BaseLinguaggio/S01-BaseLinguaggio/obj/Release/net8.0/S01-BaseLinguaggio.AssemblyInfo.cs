394bf92252b63e2beee3b6c176c396c50ce5fccf
marzianegro <ngr.marzia@gmail.com>
2024-03-13 15:23:13 +0100

    added .gitignore

--- src/S01-BaseLinguaggio/S01-BaseLinguaggio/obj/Release/net8.0/S01-BaseLinguaggio.AssemblyInfo.cs
+++ src/S01-BaseLinguaggio/S01-BaseLinguaggio/obj/Release/net8.0/S01-BaseLinguaggio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S01-BaseLinguaggio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5869ac80937beff28c5e6db0baf8ff5e798b4381")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a174162868b9646fc4ad78f76878c0835ad6b649")]
 [assembly: System.Reflection.AssemblyProductAttribute("S01-BaseLinguaggio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S01-BaseLinguaggio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
