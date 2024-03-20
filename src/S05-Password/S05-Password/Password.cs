0febb7aae88fbcd5b17a8d5f0bd29f3d111a92e9
marzianegro <ngr.marzia@gmail.com>
2024-03-20 16:03:41 +0100

    HATED. this last exercise

---
+++ src/S05-Password/S05-Password/Password.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace S05_Password;
+
+
+public class Password
+{
+	// Using char[]
+	public string Genera(int length) {
+		string @base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789£$%&?@#";
+		char[] pw = new char[length];
+		Random rand = new();
+
+		for (int i = 0; i < length; i++) {
+			pw[i] = @base[rand.Next(@base.Length)];
+		}
+
+		return new string(pw);
+	}
+
+	// Using System.Text.StringBuilder
+	//public string Genera(int length)
+	//{
+	//	string @base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789£$%&?@#";
+	//	Random rand = new Random();
+	//	StringBuilder pw = new StringBuilder(length);
+
+	//	for (int i = 0; i < length; i++)
+	//	{
+	//		pw.Append(@base[rand.Next(@base.Length)]);
+	//	}
+
+	//	return pw.ToString();
+	//}
+}
---
+++ src/S05-Password/S05-Password/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace S05_Password;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Password password = new();
+        string pw;
+        
+        pw = password.Genera(14);
+        Console.WriteLine(pw);
+    }
+}
