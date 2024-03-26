4c22ec3cee230210c0fe012a4ec4ed9886bfce14
marzianegro <ngr.marzia@gmail.com>
2024-03-26 12:30:28 +0100

    was gardening without gardener lol

---
+++ src/S08-Giardiniere/S08-Giardiniere/Program.cs
@@ -0,0 +1,19 @@
+﻿using Geometria;
+
+namespace S08_Giardiniere;
+
+class Program
+{
+    static void Main()
+    {
+        Rettangolo prato = new(6, 7);
+        Cerchio siepe = new(2);
+
+        Preventivo preventivoMario = new();
+        preventivoMario.PreventivoPrato(prato.Area());
+        Console.WriteLine();
+        preventivoMario.PreventivoSiepe(siepe.Perimetro());
+        Console.WriteLine();
+		preventivoMario.PreventivoTotale();
+	}
+}
