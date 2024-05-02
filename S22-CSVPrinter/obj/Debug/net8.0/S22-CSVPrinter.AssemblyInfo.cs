15269db41105c771ac33c767ab03381efbf7c601
marzianegro <ngr.marzia@gmail.com>
2024-05-02 15:46:13 +0200

    fixed for clarity

--- S22-CSVPrinter/obj/Debug/net8.0/S22-CSVPrinter.AssemblyInfo.cs
+++ S22-CSVPrinter/obj/Debug/net8.0/S22-CSVPrinter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S22-csvPrinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2513414165238246f1c7e21eadc8dedb5d4a0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fc722182dbe7954edbe4dfbc50dcd1de050dd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("S22-csvPrinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S22-csvPrinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- S24-ShuntingYard/Program.cs
+++ S24-ShuntingYard/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string mathExpression = "10.22 + 32 / 4.65 * 5 ?? ="; // Parsed expression results in: 10.22 32 4.65 / 5 * +
+        string mathExpression = "10.22 + 32 / 4.65 * 5 ?? ="; 
         StringSource stringSource = new(mathExpression); // Creating the char emitter
         Filter filter = new();
 
@@ -26,10 +26,10 @@ class Program
 
         CPU cpu = new();
         // There's no need for this ⭣ anymore, as we're now attaching the cpu to the logic
-        // logic.Attach(new ObserverPrinter<object>()); // RPN
-        logic.Attach(cpu); // RPN
+        // logic.Attach(new ObserverPrinter<object>()); // NOTES: Parsed expression results in RPN expression: 10.22 32 4.65 / 5 * +
+        logic.Attach(cpu);
 
-        cpu.Attach(new ObserverPrinter<string>()); // RPN
+        cpu.Attach(new ObserverPrinter<string>());
         stringSource.Run();
     }
 }
